8282df48dde478421037d0d3f72e2c9bc4f802ba
Vladimir Edheldor Leksin <edheldor@gmail.com>
2019-04-16 19:12:48 +0300

    Добавил файл requirements.txt, создал пример в scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,16 @@
-from RBKNewsScraper import *
+from RBKNewsScraper import RBKNewsScraper, NewsItem
 
+#Создаем объект скэппера, он автоматически парсит 15 новостей с главной страницы
 news = RBKNewsScraper()
+
+#Теперь мы можем с ним работать
+
+#Вывод новостей на стандартный вывод
 news.print_all_news()
+
+#Возвращение списка новостей в виде списка со словарями
+d = news.get_list()
+
+
+#Возвращение списка новостей в виде JSON
+j = news.get_json()
